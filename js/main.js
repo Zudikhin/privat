deb78fd7cc124ee30625bd3e35a729cf2cc4eb0d
Zudikhin <uzudihin@gmail.com>
2023-09-30 23:38:29 +0300

    final

--- js/main.js
+++ js/main.js
@@ -2,6 +2,49 @@
 	'use strict';
 
 	/*==============================
+	Header
+	==============================*/
+	if (document.querySelector('.header__menu')) {
+		var headerBtn = document.querySelector('.header__menu');
+		// var headerNav = document.querySelector('.menu');
+
+		headerBtn.addEventListener('click', function() {
+			headerBtn.classList.toggle('header__menu--active');
+			// headerNav.classList.toggle('menu--active');
+		});
+	}
+
+	/*==============================
+	Hero carousel
+	==============================*/
+	if (document.querySelector('.hero__carousel')) {
+		var elms = document.getElementsByClassName('hero__carousel');
+
+		for ( var i = 0; i < elms.length; i++ ) {
+			new Splide(elms[ i ], {
+				type: 'slide',
+				perPage: 1,
+				drag: false,
+				pagination: true,
+				autoWidth: false,
+				autoHeight: false,
+				speed: 800,
+				gap: 24,
+				arrows: false,
+				focus: 0,
+				breakpoints: {
+					1199: {
+						type: 'loop',
+						arrows: false,
+						drag: true,
+						pagination: false,
+					},
+				}
+			}).mount();
+		}
+	}
+
+	/*==============================
 	Residents carousel
 	==============================*/
 	if (document.querySelector('.residents')) {
@@ -108,5 +151,39 @@
 			}).mount();
 		}
 	}
+
+	if (document.querySelector('.calendar')) {
+		$('.calendar').evoCalendar({
+			language: 'ru',
+			firstDayOfWeek: 1,
+			titleFormat: 'MM',
+			eventHeaderFormat: 'd MM yyyy',
+			sidebarToggler: true,
+			eventListToggler: false,
+			calendarEvents: [
+				{
+					id: '0',
+					name: "Венчурные инвестиции",
+					description: "Текст текст текст Текст текст текст Текст текст текст Текст текст текст Текст текст текст",
+					date: "September/30/2023",
+					type: "event",
+				},
+				{
+					id: '1',
+					name: "Онлайн-конференция Венчурные инвестиции",
+					date: "October/1/2023",
+					type: "event",
+				},
+				{
+					id: '2',
+					name: "Онлайн-конференция",
+					description: "Текст текст текст",
+					date: "October/5/2023",
+					type: "event",
+				},
+			]
+		});
+	}
+
 
 })(window, document);

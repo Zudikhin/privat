71b122a3d3a0081d3dbd5dcfaaf78ec5856d9704
Zudikhin <uzudihin@gmail.com>
2023-10-02 10:55:05 +0300

    final pls

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@
 
 		for ( var i = 0; i < elms.length; i++ ) {
 			new Splide(elms[ i ], {
-				type: 'slide',
+				type: 'loop',
 				perPage: 1,
 				drag: false,
 				pagination: true,
@@ -34,7 +34,6 @@
 				focus: 0,
 				breakpoints: {
 					1199: {
-						type: 'loop',
 						arrows: false,
 						drag: true,
 						pagination: false,
@@ -45,6 +44,16 @@
 	}
 
 	/*==============================
+	Counter
+	==============================*/
+	if (document.querySelector('.club')) {
+		$('.club__number span').counterUp({
+			delay: 10,
+			time: 800
+		});
+	}
+
+	/*==============================
 	Residents carousel
 	==============================*/
 	if (document.querySelector('.residents')) {
@@ -52,7 +61,7 @@
 
 		for ( var i = 0; i < elms.length; i++ ) {
 			new Splide(elms[ i ], {
-				type: 'slide',
+				type: 'loop',
 				perPage: 3,
 				drag: false,
 				pagination: false,
@@ -64,21 +73,18 @@
 				focus: 0,
 				breakpoints: {
 					767: {
-						type: 'loop',
 						arrows: false,
 						drag: true,
 						perPage: 1,
 						gap: 24,
 					},
 					991: {
-						type: 'loop',
 						arrows: false,
 						drag: true,
 						perPage: 2,
 						gap: 24,
 					},
 					1199: {
-						type: 'loop',
 						arrows: false,
 						drag: true,
 						perPage: 2,
@@ -152,38 +158,178 @@
 		}
 	}
 
+	/*==============================
+	Calendar
+	==============================*/
 	if (document.querySelector('.calendar')) {
-		$('.calendar').evoCalendar({
-			language: 'ru',
-			firstDayOfWeek: 1,
-			titleFormat: 'MM',
-			eventHeaderFormat: 'd MM yyyy',
-			sidebarToggler: true,
-			eventListToggler: false,
-			calendarEvents: [
+		var elms = document.getElementsByClassName('calendar');
+
+		for ( var i = 0; i < elms.length; i++ ) {
+			new Splide(elms[ i ], {
+				type: 'slide',
+				perPage: 3,
+				drag: false,
+				pagination: false,
+				autoWidth: false,
+				autoHeight: false,
+				speed: 800,
+				gap: 24,
+				arrows: false,
+				focus: 0,
+				breakpoints: {
+					767: {
+						arrows: false,
+						drag: true,
+						perPage: 1,
+						gap: 24,
+					},
+					991: {
+						arrows: false,
+						drag: true,
+						perPage: 2,
+						gap: 24,
+					},
+					1199: {
+						arrows: false,
+						drag: true,
+						perPage: 2,
+						gap: 24,
+					},
+					1399: {
+						arrows: false,
+						perPage: 2,
+						gap: 24,
+					},
+				}
+			}).mount();
+		}
+
+		$('#calendar-1').bitroidCalendarEv({
+			lang: "ru",
+			sundayFirst: false,
+			years: "2023-2024",
+			startDate: "01.10.2023",
+			format: "DD.MM.YYYY",
+			showEventBlock: false,
+			events : [
 				{
-					id: '0',
-					name: "Венчурные инвестиции",
-					description: "Текст текст текст Текст текст текст Текст текст текст Текст текст текст Текст текст текст",
-					date: "September/30/2023",
-					type: "event",
+					day : "03",
+					month : "10",
+					year : "2023",
+					event_description : "Описание события",
 				},
 				{
-					id: '1',
-					name: "Онлайн-конференция Венчурные инвестиции",
-					date: "October/1/2023",
-					type: "event",
+					day : "07",
+					month : "10",
+					year : "2023",
+					event_description : "Описание события",
 				},
 				{
-					id: '2',
-					name: "Онлайн-конференция",
-					description: "Текст текст текст",
-					date: "October/5/2023",
-					type: "event",
+					day : "20",
+					month : "10",
+					year : "2023",
+					event_description : "Описание события",
+				}
+			]
+		});
+
+		$('#calendar-2').bitroidCalendarEv({
+			lang: "ru",
+			sundayFirst: false,
+			years: "2023-2024",
+			startDate: "01.11.2023",
+			format: "DD.MM.YYYY",
+			showEventBlock: false,
+			events : [
+				{
+					day : "16",
+					month : "11",
+					year : "2023",
+					event_description : "Описание события",
 				},
+				{
+					day : "18",
+					month : "11",
+					year : "2023",
+					event_description : "Описание события",
+				},
+				{
+					day : "25",
+					month : "11",
+					year : "2023",
+					event_description : "Описание события",
+				}
+			]
+		});
+
+		$('#calendar-3').bitroidCalendarEv({
+			lang: "ru",
+			sundayFirst: false,
+			years: "2023-2024",
+			startDate: "01.12.2023",
+			format: "DD.MM.YYYY",
+			showEventBlock: false,
+			events : [
+				{
+					day : "12",
+					month : "12",
+					year : "2023",
+					event_description : "Описание события",
+				},
+				{
+					day : "19",
+					month : "12",
+					year : "2023",
+					event_description : "Описание события",
+				}
+			]
+		});
+
+		$('#calendar-4').bitroidCalendarEv({
+			lang: "ru",
+			sundayFirst: false,
+			years: "2023-2024",
+			startDate: "01.01.2024",
+			format: "DD.MM.YYYY",
+			showEventBlock: false,
+			events : [
+				{
+					day : "03",
+					month : "01",
+					year : "2024",
+					event_description : "Описание события",
+				},
+				{
+					day : "05",
+					month : "01",
+					year : "2024",
+					event_description : "Описание события",
+				}
+			]
+		});
+
+		$('#calendar-5').bitroidCalendarEv({
+			lang: "ru",
+			sundayFirst: false,
+			years: "2023-2024",
+			startDate: "01.02.2024",
+			format: "DD.MM.YYYY",
+			showEventBlock: false,
+			events : [
+				{
+					day : "03",
+					month : "02",
+					year : "2024",
+					event_description : "Описание события",
+				},
+				{
+					day : "05",
+					month : "02",
+					year : "2024",
+					event_description : "Описание события",
+				}
 			]
 		});
 	}
-
 
 })(window, document);
